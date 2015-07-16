a34e81030cacf02a48f2982236b2dde65b3b0fe6
Noah Kantrowitz <noah@coderanger.net>
2015-07-16 15:46:07 -0700

    New resource: python_requirements.
    Handles installing a requirements.txt.

---
+++ lib/poise_python/resources/python_requirements.rb
@@ -0,0 +1,98 @@
+#
+# Copyright 2015, Noah Kantrowitz
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'chef/provider'
+require 'chef/resource'
+require 'poise'
+
+require 'poise_python/python_command_mixin'
+
+
+module PoisePython
+  module Resources
+    # (see PythonRequirements::Resource)
+    # @since 1.0.0
+    module PythonRequirements
+      # A `python_requirements` resource to manage Python virtual environments.
+      #
+      # @provides python_requirements
+      # @action create
+      # @action delete
+      # @example
+      #   python_requirements '/opt/myapp/requirements.txt'
+      class Resource < Chef::Resource
+        include PoisePython::PythonCommandMixin
+        provides(:python_requirements)
+        actions(:install, :upgrade)
+
+        attribute(:path, kind_of: String, name_attribute: true)
+      end
+
+      # The default provider for `python_requirements`.
+      #
+      # @see Resource
+      # @provides python_requirements
+      class Provider < Chef::Provider
+        include Poise
+        include PoisePython::PythonCommandMixin
+        provides(:python_requirements)
+
+        # The `install` action for the `python_requirements` resource.
+        #
+        # @return [void]
+        def action_install
+          install_requirements(upgrade: false)
+        end
+
+        # The `upgrade` action for the `python_requirements` resource.
+        #
+        # @return [void]
+        def action_ugrade
+          install_requirements(upgrade: true)
+        end
+
+        private
+
+        # Run an install --requirements command and parse the output.
+        #
+        # @param upgrade [Boolean] If we should use the --upgrade flag.
+        # @return [void]
+        def install_requirements(upgrade: false)
+          cmd = %w{-m pip install}
+          cmd << '--upgrade' if upgrade
+          cmd << '--requirement'
+          cmd << requirements_path
+          output = python_shell_out!(cmd).stdout
+          if output.include?('Successfully installed')
+            new_resource.updated_by_last_action(true)
+          end
+        end
+
+        # Find the true path to the requirements file.
+        #
+        # @return [String]
+        def requirements_path
+          if ::File.directory?(new_resource.path)
+            ::File.join(new_resource.path, 'requirements.txt')
+          else
+            new_resource.path
+          end
+        end
+
+      end
+    end
+  end
+end
