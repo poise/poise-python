d0cf861aa58abd4ce619972b381babdc3ea07737
Noah Kantrowitz <noah@coderanger.net>
2015-08-23 18:27:51 -0500

    Test coverage for the portable pypy provider.

---
+++ test/spec/python_providers/portable_pypy_spec.rb
@@ -0,0 +1,70 @@
+#
+# Copyright 2015, Noah Kantrowitz
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'spec_helper'
+
+describe PoisePython::PythonProviders::PortablePyPy do
+  let(:python_version) { nil }
+  let(:chefspec_options) { {platform: 'ubuntu', version: '14.04'} }
+  let(:default_attributes) { {poise_python_version: python_version} }
+  let(:python_runtime) { chef_run.python_runtime('test') }
+  step_into(:python_runtime)
+  recipe do
+    python_runtime 'test' do
+      version node['poise_python_version']
+      virtualenv_version false
+    end
+  end
+
+  context 'with version pypy' do
+    let(:python_version) { 'pypy' }
+    it { expect(python_runtime.provider_for_action(:install)).to be_a described_class }
+    it { is_expected.to create_remote_file(File.join(Chef::Config[:file_cache_path], 'pypy-2.6-linux_x86_64-portable.tar.bz2')).with(source: 'https://bitbucket.org/squeaky/portable-pypy/downloads/pypy-2.6-linux_x86_64-portable.tar.bz2') }
+    it { expect(python_runtime.python_binary).to eq '/opt/pypy-2.6-linux_x86_64-portable/bin/pypy'}
+  end # /context with version pypy
+
+  context 'with version pypy-2.4' do
+    let(:python_version) { 'pypy-2.4' }
+    it { expect(python_runtime.provider_for_action(:install)).to be_a described_class }
+    it { is_expected.to create_remote_file(File.join(Chef::Config[:file_cache_path], 'pypy-2.4-linux_x86_64-portable.tar.bz2')).with(source: 'https://bitbucket.org/squeaky/portable-pypy/downloads/pypy-2.4-linux_x86_64-portable.tar.bz2') }
+    it { expect(python_runtime.python_binary).to eq '/opt/pypy-2.4-linux_x86_64-portable/bin/pypy'}
+  end # /context with version pypy-2.4
+
+  context 'with version pypy3' do
+    let(:python_version) { 'pypy3' }
+    it { expect(python_runtime.provider_for_action(:install)).to be_a described_class }
+    it { is_expected.to create_remote_file(File.join(Chef::Config[:file_cache_path], 'pypy3-2.4-linux_x86_64-portable.tar.bz2')).with(source: 'https://bitbucket.org/squeaky/portable-pypy/downloads/pypy3-2.4-linux_x86_64-portable.tar.bz2') }
+    it { expect(python_runtime.python_binary).to eq '/opt/pypy3-2.4-linux_x86_64-portable/bin/pypy'}
+  end # /context with version pypy3
+
+  context 'with version pypy3-2.3.1' do
+    let(:python_version) { 'pypy3-2.3.1' }
+    it { expect(python_runtime.provider_for_action(:install)).to be_a described_class }
+    it { is_expected.to create_remote_file(File.join(Chef::Config[:file_cache_path], 'pypy3-2.3.1-linux_x86_64-portable.tar.bz2')).with(source: 'https://bitbucket.org/squeaky/portable-pypy/downloads/pypy3-2.3.1-linux_x86_64-portable.tar.bz2') }
+    it { expect(python_runtime.python_binary).to eq '/opt/pypy3-2.3.1-linux_x86_64-portable/bin/pypy'}
+  end # /context with version pypy3-2.3.1
+
+  context 'action :uninstall' do
+    recipe do
+      python_runtime 'test' do
+        version 'pypy'
+        action :uninstall
+      end
+    end
+
+    it { is_expected.to delete_directory('/opt/pypy-2.6-linux_x86_64-portable').with(recursive: true) }
+  end # /context action :uninstall
+end
