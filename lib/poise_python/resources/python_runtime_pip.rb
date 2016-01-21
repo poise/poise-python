0a7d0ef9253099795a5c77126318f38d31d28783
Noah Kantrowitz <noah@coderanger.net>
2016-01-20 21:25:03 -0800

    Support for bootstrapping with a specific version of pip.
    /cc @pidydx

--- lib/poise_python/resources/python_runtime_pip.rb
+++ lib/poise_python/resources/python_runtime_pip.rb
@@ -104,10 +104,6 @@ module PoisePython
         def bootstrap_pip
           # Always updated if we have hit this point.
           new_resource.updated_by_last_action(true)
-          # Pending https://github.com/pypa/pip/issues/1087.
-          if new_resource.version
-            Chef::Log.warn("pip does not support bootstrapping a specific version, see https://github.com/pypa/pip/issues/1087.")
-          end
           # Use a temp file to hold the installer.
           Tempfile.create(['get-pip', '.py']) do |temp|
             # Download the get-pip.py.
@@ -120,12 +116,15 @@ module PoisePython
             # al. Disable setuptools and wheel as we will install those later.
             # Use the environment vars instead of CLI arguments so I don't have
             # to deal with bootstrap versions that don't support --no-wheel.
-            poise_shell_out!([new_resource.parent.python_binary, temp.path], environment: new_resource.parent.python_environment.merge('PIP_NO_SETUPTOOLS' => '1', 'PIP_NO_WHEEL' => '1'))
+            boostrap_cmd = [new_resource.parent.python_binary, temp.path]
+            boostrap_cmd << "pip==#{new_resource.version}" if new_resource.version
+            poise_shell_out!(boostrap_cmd, environment: new_resource.parent.python_environment.merge('PIP_NO_SETUPTOOLS' => '1', 'PIP_NO_WHEEL' => '1'))
           end
           new_pip_version = pip_version
           if new_resource.version && new_pip_version != new_resource.version
             # We probably want to downgrade, which is silly but ¯\_(ツ)_/¯.
             # Can be removed once https://github.com/pypa/pip/issues/1087 is fixed.
+            # That issue is fixed, leaving a bit longer for older vendored scripts.
             current_resource.version(new_pip_version)
             install_pip
           end
